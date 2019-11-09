b278b2a3b51e0560aaef701cd4c8f9714eeb479e
Cristian Greco <cristianrgreco@gmail.com>
2019-11-09 15:01:17 +0000

    Minor cleanup

--- src/RecipePreview.js
+++ src/RecipePreview.js
@@ -20,35 +20,41 @@ export default function RecipePreview({recipe}) {
                     <img src={recipe.image} className="responsive-img"/>
                 </div>
             </div>
-            {recipe.equipment.length > 0 && (
-                <div className="section">
-                    <h3 className="header">Equipment</h3>
-                    <ul className="collection">
-                        {recipe.equipment.map(item =>
-                            <li key={item} className="collection-item">
-                                <label>
-                                    <input type="checkbox"/>
-                                    <span>{item}</span>
-                                </label>
-                            </li>
-                        )}
-                    </ul>
-                </div>
-            )}
-            <div className="section">
-                <h3 className="header">Ingredients</h3>
-                <ul className="collection">
-                    {recipe.ingredients.map(ingredient =>
-                        <li key={ingredient} className="collection-item">
-                            <label>
-                                <input type="checkbox"/>
-                                <span>{ingredient}</span>
-                            </label>
-                        </li>
-                    )}
-                </ul>
+            <div className="row">
+                {recipe.equipment.length > 0 && (
+                    <Fragment>
+                        <h3 className="header">Equipment</h3>
+                        <ul className="collection">
+                            {recipe.equipment.map(item =>
+                                <li key={item} className="collection-item">
+                                    <label>
+                                        <input type="checkbox"/>
+                                        <span>{item}</span>
+                                    </label>
+                                </li>
+                            )}
+                        </ul>
+                    </Fragment>
+                )}
             </div>
-            <div className="section">
+            <div className="row">
+                {recipe.ingredients.length > 0 && (
+                    <Fragment>
+                        <h3 className="header">Ingredients</h3>
+                        <ul className="collection">
+                            {recipe.ingredients.map(ingredient =>
+                                <li key={ingredient} className="collection-item">
+                                    <label>
+                                        <input type="checkbox"/>
+                                        <span>{ingredient}</span>
+                                    </label>
+                                </li>
+                            )}
+                        </ul>
+                    </Fragment>
+                )}
+            </div>
+            <div className="row">
                 <Link to={`/recipes/${recipe._id}/start`} className="waves-effect waves-light red lighten-2 btn-large">
                     <i className="material-icons left">sentiment_very_satisfied</i>
                     I'm ready!
