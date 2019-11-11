37f8091ab2ed29f5c3c118a076ffbcc2150f796a
Cristian Greco <cristianrgreco@gmail.com>
2019-11-11 22:46:15 +0000

    Remove config

--- src/Nav.js
+++ src/Nav.js
@@ -2,7 +2,6 @@ import React, {Fragment, useState} from 'react';
 import {Link, useHistory} from "react-router-dom";
 import styles from './Nav.module.css';
 import {Icon} from "./components/Icon";
-import config from './config';
 import {logout} from "./auth";
 
 export default function Nav({loggedIn, setLoggedIn}) {
@@ -22,7 +21,7 @@ export default function Nav({loggedIn, setLoggedIn}) {
             <nav className={styles.Nav}>
                 <div className={`${styles.Nav_Wrapper} nav-wrapper`}>
                     <div className="container">
-                        <Link to="/" className={`${styles.Nav_Wrapper_Logo} brand-logo`}>{config.title}</Link>
+                        <Link to="/" className={`${styles.Nav_Wrapper_Logo} brand-logo`}>Hello, Diners</Link>
                         <a href="#" data-target="mobile" className={`${styles.Nav_Wrapper_SidenavTrigger} sidenav-trigger`}>
                             <Icon name="menu"/>
                         </a>
@@ -51,7 +50,7 @@ function NavLinks({history, isMobile, loggedIn, setLoggedIn, logout}) {
             {isMobile && (
                 <li className="logo">
                     <a href="/" className={`${styles.Nav_Wrapper_Sidenav_LogoContainer} brand-logo`}>
-                        <h1 className={styles.Nav_Wrapper_Sidenav_LogoContainer_Logo}>{config.title}</h1>
+                        <h1 className={styles.Nav_Wrapper_Sidenav_LogoContainer_Logo}>Hello, Diners</h1>
                     </a>
                 </li>
             )}
