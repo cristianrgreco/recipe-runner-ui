46a9482596b6b66eeac23ef53529fd64dde3f113
Cristian Greco <cristianrgreco@gmail.com>
2020-10-18 20:39:18 +0100

    Highlight active route in navbar

--- src/Nav.js
+++ src/Nav.js
@@ -1,11 +1,12 @@
 import React, { Fragment, useEffect } from "react";
-import { Link, useHistory } from "react-router-dom";
+import { Link, useHistory, useLocation } from "react-router-dom";
 import styles from "./Nav.module.css";
 import { Icon } from "./components/Icon";
 import { logout } from "./auth";
 
 export default function Nav({ loggedIn, setLoggedIn }) {
   const history = useHistory();
+  const location = useLocation();
 
   useEffect(() => {
     window.M.Sidenav.init(document.querySelectorAll(".sidenav"));
@@ -25,6 +26,7 @@ export default function Nav({ loggedIn, setLoggedIn }) {
             <ul className="right hide-on-med-and-down">
               <NavLinks
                 history={history}
+                location={location}
                 isMobile={false}
                 loggedIn={loggedIn}
                 setLoggedIn={setLoggedIn}
@@ -35,13 +37,22 @@ export default function Nav({ loggedIn, setLoggedIn }) {
         </div>
       </nav>
       <ul id="mobile" className="sidenav sidenav-close">
-        <NavLinks history={history} isMobile={true} loggedIn={loggedIn} setLoggedIn={setLoggedIn} logout={logout} />
+        <NavLinks
+          history={history}
+          location={location}
+          isMobile={true}
+          loggedIn={loggedIn}
+          setLoggedIn={setLoggedIn}
+          logout={logout}
+        />
       </ul>
     </header>
   );
 }
 
-function NavLinks({ history, isMobile, loggedIn, setLoggedIn, logout }) {
+function NavLinks({ history, location, isMobile, loggedIn, setLoggedIn, logout }) {
+  const isActive = (pathname) => location.pathname === pathname;
+
   const onLogout = () => {
     logout();
     setLoggedIn(false);
@@ -58,14 +69,14 @@ function NavLinks({ history, isMobile, loggedIn, setLoggedIn, logout }) {
         </li>
       )}
       <li>
-        <Link to="/" className={styles.Nav_Wrapper_Sidenav_Link}>
-          Recipes
+        <Link to="/" className={`${styles.Nav_Wrapper_Sidenav_Link} ${isActive("/") && styles.Active}`}>
+          Home
         </Link>
       </li>
       {loggedIn ? (
         <Fragment>
           <li>
-            <Link to="/recipe-editor" className={styles.Nav_Wrapper_Sidenav_Link}>
+            <Link to="/recipe-editor" className={`${styles.Nav_Wrapper_Sidenav_Link}`}>
               Create
             </Link>
           </li>
@@ -78,12 +89,15 @@ function NavLinks({ history, isMobile, loggedIn, setLoggedIn, logout }) {
       ) : (
         <Fragment>
           <li>
-            <Link to="/login" className={styles.Nav_Wrapper_Sidenav_Link}>
+            <Link to="/login" className={`${styles.Nav_Wrapper_Sidenav_Link} ${isActive("/login") && styles.Active}`}>
               Login
             </Link>
           </li>
           <li>
-            <Link to="/register" className={styles.Nav_Wrapper_Sidenav_Link}>
+            <Link
+              to="/register"
+              className={`${styles.Nav_Wrapper_Sidenav_Link} ${isActive("/register") && styles.Active}`}
+            >
               Register
             </Link>
           </li>
